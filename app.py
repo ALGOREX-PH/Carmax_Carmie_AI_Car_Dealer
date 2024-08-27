328e1fa7fb06a4322498601f3b14f994e633bfdd
Danielle Meer <55391597+TIGERTIDES@users.noreply.github.com>
2024-08-27 23:10:02 +0800

    Login

--- app.py
+++ app.py
@@ -21,63 +21,83 @@ from streamlit_extras.mention import mention
 # LinkedIn : https://www.linkedin.com/in/algorexph/
 
 warnings.filterwarnings("ignore")
-st.set_page_config(page_title="Carmie by Generative Labs", page_icon=":car:", layout="wide")
+st.set_page_config(page_title="Carmie : The AI Car Dealer", page_icon=":car:", layout="wide")
 
-with st.sidebar :
-    st.image("https://raw.githubusercontent.com/ALGOREX-PH/Carmax_Carmie_AI_Car_Dealer/main/Images/carmax-logo.webp")
-    openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-    if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
-        st.warning('Please enter your OpenAI API token!', icon='⚠️')
-    else:
-        st.success('Proceed to entering your prompt message!', icon='👉')
-    with st.container() :
-        l, m, r = st.columns((1, 3, 1))
-        with l : st.empty()
-        with m : st.empty()
-        with r : st.empty()
+VALID_USERNAME = "Genlabs"
+VALID_PASSWORD = "FutureWrittenInCod3"
 
-    options = option_menu(
-        "Dashboard", 
-        ["Home", "Model"],
-        icons = ['house', 'tools'],
-        menu_icon = "book", 
-        default_index = 0,
-        styles = {
-            "icon" : {"color" : "#dec960", "font-size" : "20px"},
-            "nav-link" : {"font-size" : "17px", "text-align" : "left", "margin" : "5px", "--hover-color" : "#262730"},
-            "nav-link-selected" : {"background-color" : "#262730"}          
-        })
+def login() :
+    st.sidebar.title("Login")
+    username = st.sidebar.text_input("Username")
+    password = st.sidebar.text_input("Password", type = "password")
 
+    if st.sidebar.title("Login") :
+        if username == VALID_USERNAME and password == VALID_PASSWORD :
+           st.sidebar.success("Login successful!")
+           st.session_state["authenticated"] = True
+           st.experimental_rerun()  # Rerun the app to proceed after login
+        else : 
+           st.sidebar.error("Invalid username or password")
 
-if options == "Home" :
-   st.title("Introducing Carmie: Carmax's AI Car Dealer")
-   st.write("We are thrilled to introduce you to Carmie, Carmax's very own AI Car Dealer. Carmie is here to revolutionize your car-buying experience with cutting-edge technology and unparalleled convenience.")
-   st.write("# Who is Carmie?")
-   st.write("Carmie is an intelligent AI-powered assistant designed to make your car shopping journey smooth, personalized, and enjoyable. Whether you’re looking to buy your first car, upgrade to a newer model, or simply explore options, Carmie is here to assist you every step of the way.")
-   st.write("# How Can Carmie Help You?")
-   st.write("1. Personalized Recommendations: Carmie analyzes your preferences, budget, and needs to recommend the best vehicles for you.")
-   st.write("2. Instant Information: Get detailed information on car models, features, prices, and availability in real-time.")
-   st.write("3. Financing Assistance: Carmie provides guidance on financing options and helps you find the best deals.")
-   st.write("# Why Choose Carmie?")
-   st.write("- 24/7 Availability: Carmie is always ready to help, anytime, anywhere.")
-   st.write("- Expert Knowledge: Benefit from Carmie’s extensive knowledge of all car models and features.")
-   st.write("- Hassle-Free Experience: Skip the dealership visit and manage everything online with ease.")
-   st.write("- Customer-Centric Approach: Carmie focuses on your needs and provides a tailored car-buying journey.")
-   st.write("# Join the Future of Car Shopping")
-   st.write("With Carmie, Carmax is taking a bold step into the future of car shopping. Embrace the convenience, efficiency, and personalization that only an AI-driven assistant can offer. Discover your perfect car today with Carmie!")
+if "authenticated" not in st.session_state :
+   st.session_state["authenticated"] = False
+   
+if st.session_state["authenticated"] :
+   with st.sidebar :
+        st.image("https://raw.githubusercontent.com/ALGOREX-PH/Carmax_Carmie_AI_Car_Dealer/main/Images/carmax-logo.webp")
+        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
+        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
+           st.warning('Please enter your OpenAI API token!', icon='⚠️')
+        else:
+           st.success('Proceed to entering your prompt message!', icon='👉')
+        with st.container() :
+           l, m, r = st.columns((1, 3, 1))
+           with l : st.empty()
+           with m : st.empty()
+           with r : st.empty()
 
-elif options == "Model" :
-     dataframed = pd.read_excel('https://raw.githubusercontent.com/ALGOREX-PH/Carmax_Carmie_AI_Car_Dealer/main/Dataset/Carmax%20Inventory%207_6_24.xlsx')
-     dataframed['Car_Title'] = dataframed['make'] + "_" + dataframed['model']
-     dataframed['combined'] = dataframed.apply(lambda row : ' '.join(row.values.astype(str)), axis = 1)
-     documents = dataframed['combined'].tolist()
-     embeddings = [get_embedding(doc, engine = "text-embedding-ada-002") for doc in documents]
-     embedding_dim = len(embeddings[0])
-     embeddings_np = np.array(embeddings).astype('float32')
-     index = faiss.IndexFlatL2(embedding_dim)
-     index.add(embeddings_np)
+        options = option_menu(
+           "Dashboard", 
+           ["Home", "Model"],
+           icons = ['house', 'tools'],
+           menu_icon = "book", 
+           default_index = 0,
+           styles = {
+              "icon" : {"color" : "#dec960", "font-size" : "20px"},
+              "nav-link" : {"font-size" : "17px", "text-align" : "left", "margin" : "5px", "--hover-color" : "#262730"},
+              "nav-link-selected" : {"background-color" : "#262730"}          
+           })
+        
 
-     System_Prompt = """
+   if options == "Home" :
+      st.title("Introducing Carmie: Carmax's AI Car Dealer")
+      st.write("We are thrilled to introduce you to Carmie, Carmax's very own AI Car Dealer. Carmie is here to revolutionize your car-buying experience with cutting-edge technology and unparalleled convenience.")
+      st.write("# Who is Carmie?")
+      st.write("Carmie is an intelligent AI-powered assistant designed to make your car shopping journey smooth, personalized, and enjoyable. Whether you’re looking to buy your first car, upgrade to a newer model, or simply explore options, Carmie is here to assist you every step of the way.")
+      st.write("# How Can Carmie Help You?")
+      st.write("1. Personalized Recommendations: Carmie analyzes your preferences, budget, and needs to recommend the best vehicles for you.")
+      st.write("2. Instant Information: Get detailed information on car models, features, prices, and availability in real-time.")
+      st.write("3. Financing Assistance: Carmie provides guidance on financing options and helps you find the best deals.")
+      st.write("# Why Choose Carmie?")
+      st.write("- 24/7 Availability: Carmie is always ready to help, anytime, anywhere.")
+      st.write("- Expert Knowledge: Benefit from Carmie’s extensive knowledge of all car models and features.")
+      st.write("- Hassle-Free Experience: Skip the dealership visit and manage everything online with ease.")
+      st.write("- Customer-Centric Approach: Carmie focuses on your needs and provides a tailored car-buying journey.")
+      st.write("# Join the Future of Car Shopping")
+      st.write("With Carmie, Carmax is taking a bold step into the future of car shopping. Embrace the convenience, efficiency, and personalization that only an AI-driven assistant can offer. Discover your perfect car today with Carmie!")
+
+   elif options == "Model" :
+      dataframed = pd.read_excel('https://raw.githubusercontent.com/ALGOREX-PH/Carmax_Carmie_AI_Car_Dealer/main/Dataset/Carmax%20Inventory%207_6_24.xlsx')
+      dataframed['Car_Title'] = dataframed['make'] + "_" + dataframed['model']
+      dataframed['combined'] = dataframed.apply(lambda row : ' '.join(row.values.astype(str)), axis = 1)
+      documents = dataframed['combined'].tolist()
+      embeddings = [get_embedding(doc, engine = "text-embedding-ada-002") for doc in documents]
+      embedding_dim = len(embeddings[0])
+      embeddings_np = np.array(embeddings).astype('float32')
+      index = faiss.IndexFlatL2(embedding_dim)
+      index.add(embeddings_np)
+
+      System_Prompt = """
 You are Carmie, an AI Car Dealer for the online Car Dealership - Carmax PH.
 
 Your role is to assist customers in browsing products, providing information, and guiding them through the checkout process. Be friendly and helpful in your interactions.
@@ -103,34 +123,37 @@ Carmie: Great choice po! Meron kaming mga SUVs na swak sa preferences niyo. We h
 Feel free to ask customers about their preferences in cars, recommend possible vehicles they may like, and inform them about any ongoing promotions. Make the shopping experience enjoyable and encourage customers to reach out if they have any questions or need assistance. """
 
 
-     def initialize_conversation(prompt):
-         if 'messages' not in st.session_state:
-            st.session_state.messages = []
-            st.session_state.messages.append({"role": "system", "content": System_Prompt})
-            chat =  openai.ChatCompletion.create(model = "ft:gpt-3.5-turbo-0125:personal:carmie-3:9sTpPqlH", messages = st.session_state.messages, temperature=0.5, max_tokens=1500, top_p=1, frequency_penalty=0, presence_penalty=0)
-            response = chat.choices[0].message.content
-            st.session_state.messages.append({"role": "assistant", "content": response})
+      def initialize_conversation(prompt):
+          if 'messages' not in st.session_state:
+             st.session_state.messages = []
+             st.session_state.messages.append({"role": "system", "content": System_Prompt})
+             chat =  openai.ChatCompletion.create(model = "ft:gpt-3.5-turbo-0125:personal:carmie-3:9sTpPqlH", messages = st.session_state.messages, temperature=0.5, max_tokens=1500, top_p=1, frequency_penalty=0, presence_penalty=0)
+             response = chat.choices[0].message.content
+             st.session_state.messages.append({"role": "assistant", "content": response})
 
-     initialize_conversation(System_Prompt)
+      initialize_conversation(System_Prompt)
 
-     for messages in st.session_state.messages :
-         if messages['role'] == 'system' : continue 
-         else :
-           with st.chat_message(messages["role"]):
-                st.markdown(messages["content"])
+      for messages in st.session_state.messages :
+          if messages['role'] == 'system' : continue 
+          else :
+            with st.chat_message(messages["role"]):
+                 st.markdown(messages["content"])
 
-     if user_message := st.chat_input("Say something"):
-        with st.chat_message("user"):
-             st.markdown(user_message)
-        query_embedding = get_embedding(user_message, engine='text-embedding-ada-002')
-        query_embedding_np = np.array([query_embedding]).astype('float32')
-        _, indices = index.search(query_embedding_np, 5)
-        retrieved_docs = [documents[i] for i in indices[0]]
-        context = ' '.join(retrieved_docs)
-        structured_prompt = f"Context:\n{context}\n\nQuery:\n{user_message}\n\nResponse:"
-        chat =  openai.ChatCompletion.create(model = "ft:gpt-3.5-turbo-0125:personal:carmie-3:9sTpPqlH", messages = st.session_state.messages + [{"role": "user", "content": structured_prompt}], temperature=0.5, max_tokens=1500, top_p=1, frequency_penalty=0, presence_penalty=0)
-        st.session_state.messages.append({"role": "user", "content": user_message})
-        response = chat.choices[0].message.content
-        with st.chat_message("assistant"):
-             st.markdown(response)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+      if user_message := st.chat_input("Say something"):
+         with st.chat_message("user"):
+              st.markdown(user_message)
+         query_embedding = get_embedding(user_message, engine='text-embedding-ada-002')
+         query_embedding_np = np.array([query_embedding]).astype('float32')
+         _, indices = index.search(query_embedding_np, 5)
+         retrieved_docs = [documents[i] for i in indices[0]]
+         context = ' '.join(retrieved_docs)
+         structured_prompt = f"Context:\n{context}\n\nQuery:\n{user_message}\n\nResponse:"
+         chat =  openai.ChatCompletion.create(model = "ft:gpt-3.5-turbo-0125:personal:carmie-3:9sTpPqlH", messages = st.session_state.messages + [{"role": "user", "content": structured_prompt}], temperature=0.5, max_tokens=1500, top_p=1, frequency_penalty=0, presence_penalty=0)
+         st.session_state.messages.append({"role": "user", "content": user_message})
+         response = chat.choices[0].message.content
+         with st.chat_message("assistant"):
+              st.markdown(response)
+         st.session_state.messages.append({"role": "assistant", "content": response})
+
+else :
+    login()
